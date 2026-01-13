38303f889886721c0a30e534090e412796a26b9f
Shreyansh-32 <thakurshreyansh0609@gmail.com>
2026-01-13 15:20:35 +0530

    Frontend changes

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000/api',
+  baseURL: "/api",
   withCredentials: true, 
 });
 
